9e2778c4843628b2cc0afa677e162e9b43ae81f1
rahulvishwakarm <rahulvishwakarma6189@gmail.com>
2021-04-20 22:39:03 +0530

    Packages Updated

--- dataroidui/src/utils/Header/Header.js
+++ dataroidui/src/utils/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Header.css';
+import {Link} from 'react-router-dom';
 
 function Header() {
     return (
@@ -42,7 +43,7 @@ function Header() {
                         </li>
                         </ul>
                     </div>
-                    <button className="btn btn-primary" type="submit">Login</button>
+                    <Link to="/"><button className="btn btn-primary" type="submit">Login</button></Link>
                 </div>
             </nav>
         </div>
