9e0c11b7fd67028760614ec4f19731fd49e95308
rahulvishwakarm <rahulvishwakarma6189@gmail.com>
2021-04-20 23:54:09 +0530

    Verified Data Category and Sub Category Added

--- dataroidui/src/utils/VerifiedData/VerifiedData.js
+++ dataroidui/src/utils/VerifiedData/VerifiedData.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import './VerifiedData.css';
 
 function VerifiedData() {
     return (
         <div>
-            Verified Data
+            <div className="B2b-category-header">
+                <h2>B2B Database Provider in India</h2>
+            </div>
+            <div className="B2b-sub-category-header">
+                <b>
+                    We Deliver Research and Verified Information for B2B Contact Database with their Decision Maker details
+                </b>
+            </div>
         </div>
     )
 }
