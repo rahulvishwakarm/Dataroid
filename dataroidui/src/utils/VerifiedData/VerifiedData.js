a5253f72a844387a6feea03d87ab7208bd8fec3e
rahulvishwakarm <rahulvishwakarma6189@gmail.com>
2021-04-21 21:04:00 +0530

    Added Verified Database widget

--- dataroidui/src/utils/VerifiedData/VerifiedData.js
+++ dataroidui/src/utils/VerifiedData/VerifiedData.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import './VerifiedData.css';
 import VerifiedDataWidget from './VerifiedDataWidget';
+import img0 from '../../img/Indian-Companies-Database.png';
+import img1 from '../../img/B2B-Database-Manager.png';
+import img2 from '../../img/Indian-Industries-Database.png';
+import img3 from '../../img/Indian-Top-Companies-Database.png';
 
 function VerifiedData() {
-    const cat1 = ["IT / ITES Companies Database","Manufacturing Companies Database","BFSI Companies Database","Pharmaceutical Companies Database","Chemical Companies Database"]
+    const cat1 = [{category:"IT / ITES Companies Database"},{category:"Manufacturing Companies Database"},{category:"BFSI Companies Database"},{category:"Pharmaceutical Companies Database"},{category:"Chemical Companies Database"}];
+    const cat2 = [{category:"Healthcare Companies Database"},{category:"Textile Companies Database"},{category:"Infra Companies Database"},{category:"Logistic Companies Database"},{category:"Start-Up Companies Database"}];
+    const cat3 = [{category:"Top Companies Database"},{category:"Top Listed Companies Database"},{category:"Blue Chip Companies Database"},{category:"MNC Companies Database"},{category:"SME Companies Database"}];
+    const cat4 = [{category:"CEO Contacts Database"},{category:"IT Contact Database"},{category:"Finance Contact Database"},{category:"HR Contact Database"},{category:"Marketing Contact Database"}];
+    const image = [img0,img1,img2,img3]
     return (
         <div>
             <div className="B2b-category-header">
@@ -15,9 +23,14 @@ function VerifiedData() {
                 </p>
             </div>
             <div className="row1-widget">
-                <VerifiedDataWidget/>
-                <VerifiedDataWidget />
-                <VerifiedDataWidget/>
+                <span><VerifiedDataWidget img={image[0]} tst={cat1}/></span>
+                <span><VerifiedDataWidget img={image[1]} tst={cat2}/></span>
+                <span><VerifiedDataWidget img={image[2]} tst={cat3}/></span>
+                <span><VerifiedDataWidget img={image[3]} tst={cat4}/></span>
+                <span><VerifiedDataWidget img={image[0]} tst={cat1}/></span>
+                <span><VerifiedDataWidget img={image[1]} tst={cat2}/></span>
+                <span><VerifiedDataWidget img={image[2]} tst={cat3}/></span>
+                <span><VerifiedDataWidget img={image[3]} tst={cat4}/></span>
             </div>
         </div>
     )
--- dataroidui/src/utils/VerifiedData/VerifiedDataWidget.js
+++ dataroidui/src/utils/VerifiedData/VerifiedDataWidget.js
@@ -3,29 +3,19 @@ import IndianCompaniesDatabase from '../../img/Indian-Companies-Database.png';
 import './VerifiedDataWidget.css';
 import { AiOutlineApi } from "react-icons/ai";
 
-let categortlist1 = [
-    {category1:"IT / ITES Companies Database"},
-    {category1:"Manufacturing Companies Database"},
-    {category1:"BFSI Companies Database"},
-    {category1:"Pharmaceutical Companies Database"},
-    {category1:"Chemical Companies Database"}
-]
-
 function VerifiedDataWidget(props) {
-    const test = props.catt
-    console.log(test)
     return (
-        <div className="col-3  widget-container">
-            <div className="widget-category-icon">
-                <img src={IndianCompaniesDatabase} alt="Indian Company" title="Indian Company" style={{'width':50,'height':50}}/>
-            </div>
+        <div className="widget-container">
+                <div className="widget-category-icon">
+                    <img src={props.img} alt="Indian Company" title="Indian Company" style={{'width':50,'height':50}}/>
+                </div>
             <div className="widget-catgeory-list">
                 <ul className="list-group categ">
                         {
-                            categortlist1.map(item => (
+                            props.tst.map(item => (
                                 <li key={item.id}>
                                     <AiOutlineApi/>
-                                    <span>{item.category1}</span>
+                                    <span>{item.category}</span>
                                 </li>
                             ))
                         }
