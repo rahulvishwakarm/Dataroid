acaf4fc365c9344db29f74cc82f8dcf0aa195ebf
rahulvishwakarm <rahulvishwakarma6189@gmail.com>
2021-04-23 23:18:41 +0530

    AboutUs Initialized

--- dataroidui/src/App.js
+++ dataroidui/src/App.js
@@ -4,17 +4,27 @@ import FooterPage from "./utils/Footer/FooterPage";
 import Header from "./utils/Header/Header";
 import ServicesBanner from "./utils/ServicesBanner/ServicesBanner";
 import VerifiedData from "./utils/VerifiedData/VerifiedData";
+import {BrowserRouter as Router,Switch, Route,Link} from 'react-router-dom'
+import AboutUsMain from "./utils/AboutUs/AboutUsMain";
 
 function App() {
   return (
-    <div className="App">
-        <Header/>
-        <BannerDisplay/>
-        <Classification/>
-        <VerifiedData/>
-        <ServicesBanner/>
-        <FooterPage/>
-    </div>
+      <Router>
+        <div className="App">
+          <Header/>
+            <Route path="/">
+              <BannerDisplay/>
+              <Classification/>
+              <VerifiedData/>
+              <ServicesBanner/>
+            </Route>
+
+            <Route path="/aboutUs">
+              <AboutUsMain/>
+            </Route>
+          <FooterPage/>
+      </div>
+    </Router>
   );
 }
 
---
+++ dataroidui/src/utils/AboutUs/AboutUsMain.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AboutUsMain() {
+    return (
+        <div>
+            About Us
+        </div>
+    )
+}
+
+export default AboutUsMain;
