221c5ab6e52cddf826a5a8ead4df0f3c5e3dcbff
rahulvishwakarm <rahulvishwakarma6189@gmail.com>
2021-04-24 00:16:46 +0530

    About US Update 1

--- dataroidui/src/utils/AboutUs/AboutUsMain.js
+++ dataroidui/src/utils/AboutUs/AboutUsMain.js
@@ -3,8 +3,14 @@ import './AboutUsMain.css';
 
 function AboutUsMain() {
     return (
-        <div>
-            <h1>About Us</h1>
+        <div className="AboutUsMain">
+            <div className="AboutUs-Heading">
+                <h2>About Us</h2>
+            </div>
+            <div className="AboutDescription">
+                <p>DataRoid is a B2B marketing agency. Data is a value to an organization and as an organization our goal is to provide the valuable data to its customer. Our database solutions like B2B research, Campaign Services, Master Data Management and more will help corporates to meet their business goals. The company provides the Data as per the requirement of the clients with the preamble to solicit, plan and strategize to achieve their short term and long-term goal. The company aspire to deliver the requisites of its clients on time with very professional, seasoned and experienced personals with their uncompromising efforts and dedication.</p>
+                <p>The companies expertise skills and marketing techniques with the blend of technology is serving pan India industries viz IT/ITES, BFSI, Telecom, Education, Healthcare and many more.</p>
+            </div>
         </div>
     )
 }
